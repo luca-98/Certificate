28d62911d58d0301f266993702604e43c30833ea
Luca <Luca.developer.98@gmail.com>
2020-08-14 19:01:43 +0700

    update script

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ for (let i = 3; i < btnLinkToLinkedin.length; i++) {
 let linkedin_details = document.querySelectorAll('.linkedin-details div');
 let output = '';
 for (let i = 0; i < linkedin_details.length; i++) {
-    output += '>>>>>> ' + linkedin_details[i].children[0].children[1].innerText + '<<<<<<\n';
+    output += '> ' + linkedin_details[i].children[0].children[1].innerText + '\n';
     output += linkedin_details[i].innerText.replace(/:\n/mg, ': ');
     output += '\n\n';
 }
+
+output = output.replace(/Name:/mg, '- Name:');
+output = output.replace(/Issuing/mg, '- Issuing');
+output = output.replace(/Issue/mg, '- Issue');
+output = output.replace(/Expiration/mg, '- Expiration');
+output = output.replace(/Credential/mg, '- Credential');
